981aeb47cb00ca9a2940e47d3c919a62f64b6391
Rafif <azkarafif502@gmail.com>
2026-04-20 10:04:46 +0700

    controller

---
+++ lib/view/HomePages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homepages extends StatefulWidget {
+  const Homepages({super.key});
+
+  @override
+  State<Homepages> createState() => _HomepagesState();
+}
+
+class _HomepagesState extends State<Homepages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
